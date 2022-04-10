e3bbcca207a749697d49449a680ad6a23106d544
Luciano Lima <luciano.lima@holu.com.br>
2022-04-09 22:57:12 -0300

    Feat - Set system colors (ligth and dark)

--- ExpanseTracker/Extensions.swift
+++ ExpanseTracker/Extensions.swift
@@ -10,4 +10,7 @@ import SwiftUI
 
 extension Color{
     static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
 }
